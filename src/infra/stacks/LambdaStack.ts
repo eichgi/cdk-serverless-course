e1b9fb0dcac2e3542de0b819cdc337a775bb519d
Hiram Guerrero <hiramg90@gmail.com>
2024-05-25 10:21:12 -0600

    section 5: serverless aws lambdas - bundling, testing and debugging

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,9 +1,10 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
-import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
 import {LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
 import {ITable} from "aws-cdk-lib/aws-dynamodb";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 
 interface LambdaStackProps extends StackProps {
   spacesTable: ITable;
@@ -11,20 +12,42 @@ interface LambdaStackProps extends StackProps {
 
 export class LambdaStack extends Stack {
 
-  public readonly helloLambdaIntegration: LambdaIntegration;
+  public readonly spacesLambdaIntegration: LambdaIntegration;
 
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    const helloLambda = new Function(this, 'HelloLambda', {
+    /*
+    const helloLambda = new NodejsFunction(this, 'HelloLambda', {
       runtime: Runtime.NODEJS_18_X,
-      handler: 'hello.main',
-      code: Code.fromAsset(path.join(__dirname, "./../..", "services")),
+      handler: 'handler',
+      entry: path.join(__dirname, "./../..", "services", "hello.ts"),
       environment: {
         TABLE_NAME: props.spacesTable.tableName
       }
     });
 
+    helloLambda.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        's3:ListAllMyBuckets',
+        's3:ListBuckets'
+      ],
+      resources: ['*'] //bad practice
+    }))
+
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
+     */
+
+    const spacesLambda = new NodejsFunction(this, 'SpacesLambda', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: (path.join(__dirname, '../..', 'services', 'spaces', 'handler.ts')),
+      environment: {
+        TABLE_NAME: props.spacesTable.tableName
+      }
+    });
+
+    this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda);
   }
 }
